0c251139ca378cf9159b9d706a3785ecb24ffef9
sicho95 <sicho95@users.noreply.github.com>
2026-08-31 22:18:07 +0200

    Passer le cache PWA en build 25

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'dlstream-static-v22';
+const CACHE = 'dlstream-static-v25';
 const APP_SHELL = [
   './',
   './index.html',
@@ -8,7 +8,6 @@ const APP_SHELL = [
   './media-detector.js',
   './offline-downloader.js',
   './candidate-observer.js',
-  './offline-ui.js',
   './ashell-title.js',
   './manifest.webmanifest',
   './icon.svg',
