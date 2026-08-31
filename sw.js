890b55dd214c3ea6515be9f6a242dbb1ab9e18ef
sicho95 <sicho95@users.noreply.github.com>
2026-08-31 11:52:14 +0200

    fix: refresh cached PWA shell after redesign

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'sichostream-static-v2';
+const CACHE = 'dlstream-static-v3';
 const APP_SHELL = [
   './',
   './index.html',
@@ -22,14 +22,30 @@ self.addEventListener('activate', (event) => {
 
 self.addEventListener('fetch', (event) => {
   if (event.request.method !== 'GET') return;
+
   const url = new URL(event.request.url);
   if (url.origin !== self.location.origin) return;
 
+  if (event.request.mode === 'navigate') {
+    event.respondWith(
+      fetch(event.request)
+        .then((response) => {
+          const copy = response.clone();
+          caches.open(CACHE).then((cache) => cache.put('./index.html', copy));
+          return response;
+        })
+        .catch(() => caches.match('./index.html'))
+    );
+    return;
+  }
+
   event.respondWith(
-    caches.match(event.request).then((cached) => cached || fetch(event.request).then((response) => {
-      const copy = response.clone();
-      caches.open(CACHE).then((cache) => cache.put(event.request, copy));
-      return response;
-    }))
+    fetch(event.request)
+      .then((response) => {
+        const copy = response.clone();
+        caches.open(CACHE).then((cache) => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
   );
 });
